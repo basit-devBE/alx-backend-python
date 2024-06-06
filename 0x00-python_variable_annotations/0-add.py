ee8d23b587611fda719446aa6d80fc8d7b6f2d11
basit <basitmohammed362@gmail.com>
2024-06-06 12:29:10 +0000

    hello

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+'''Task 0's module.
+'''
 
-# Define a function called `add` that takes two arguments,
-# `a` and `b`, both of type `float`.
-# The function returns the sum of `a` and `b` as a `float`.
+
 def add(a: float, b: float) -> float:
-    # Simply return the result of adding `a` and `b`.
+    '''Adds two floating-point numbers.
+    '''
     return a + b
