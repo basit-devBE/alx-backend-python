b3fd969924b7267dc38062804e793a68bfbcb15a
basit <basitmohammed362@gmail.com>
2024-06-06 13:01:39 +0000

    hello

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 5's module.
+'''
+
+def sum_list(input_list: list[float]) -> float:
+    '''Returns the sum of a list of floats.
+    '''
+    return float(sum(input_list))
