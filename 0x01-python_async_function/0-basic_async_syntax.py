4dc01bf127ebe0712a8b4de39dc94240a3dab976
basit <basitmohammed362@gmail.com>
2024-06-10 15:04:40 +0000

    hello

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,20 +1,13 @@
-#!/usr/bin/python3
-
-# Import the necessary modules
-# asyncio is a library to write single-threaded
-#  concurrent code using coroutines, multiplexing I/O access over sockets
-# and other resources, and implementing network clients and servers
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
 import asyncio
-# Import the random module to generate random numbers
 import random
 
 
-# Define an asynchronous function wait_random that waits for a random delay
-# The function takes an optional parameter max_delay with a default value of 10
-async def wait_random(max_delay=10):
-    # Generate a random delay between 0 and max_delay
-    delay = random.uniform(0, max_delay)
-    # Wait for the generated delay using asyncio.sleep
-    await asyncio.sleep(delay)
-    # Return the delay value
-    return delay
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
