0cb977825f19311f809a4ea4224dfcf2e7b2027e
basit <basitmohammed362@gmail.com>
2024-06-11 13:20:22 +0000

    he;llo

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Async Generator module.
+'''
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''
+    Async Generator.
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
