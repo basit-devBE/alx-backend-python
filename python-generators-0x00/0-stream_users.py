2d00ca760256669952f36e1979fd5f7f6391a467
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-12-03 18:51:04 +0000

    hello

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+#stream_users.py using generators(SQL) to stream data from a database
+import mysql.connector
+
+def stream_users(connection):
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM user_data')
+    for row in cursor:
+        yield row
+    cursor.close()
+    connection.close()
