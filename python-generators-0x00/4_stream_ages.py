4bbba50989f6ef335c4174cd1430d52f2f3e619a
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-12-04 10:50:42 +0000

    hello

---
+++ python-generators-0x00/4_stream_ages.py
@@ -0,0 +1,18 @@
+import mysql.connector
+#use python generate to aggregate the average age of users in the database
+def stream_user_age():
+    dbConfig = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'bece2018',
+        'database': 'ALX_prodev'
+    }
+    conn = mysql.connector.connect(**dbConfig)
+    cursor = conn.cursor(dictionary=True)
+    try:
+        cursor.execute('SELECT age FROM user_data')
+        for row in cursor:
+            yield row['age']
+    finally:
+        cursor.close()
+        conn.close()
