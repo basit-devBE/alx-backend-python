cf0d01d7e3689d95f6092f9b00c2cb8dde966cd2
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-12-04 10:12:39 +0000

    hello

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,19 +1,31 @@
 stream_users = __import__('0-stream_users').stream_users
+import mysql.connector
 
 def stream_users_in_batches(batch_size):
     """Stream users in batches."""
-    batch = []
-    for user in stream_users():  # Assuming stream_users() fetches user data one at a time
-        batch.append(user)
-        if len(batch) == batch_size:
+    db_config = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'bece2018',
+        'database': 'ALX_prodev'
+    }
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor(dictionary=True)
+    try:
+        cursor.execute('SELECT * FROM user_data')
+        while True:
+            batch = cursor.fetchmany(size=batch_size)
+            if not batch:
+                break
             yield batch
-            batch = []  # Reset batch after yielding
-    if batch:  # Yield any remaining users
-        yield batch
+    finally:
+        cursor.close()
+        connection.close()
+   
 
 def batch_processing(batch_size):
-    """Filters users over the age of 25."""
+    """Process batches of users."""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
-            if user['age'] > 25:
-                yield user
+            yield(user)
+        print()
