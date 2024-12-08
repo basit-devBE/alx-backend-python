d12cb93683e9ac0e21516458ab7be0ea0467a4b7
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-12-08 12:38:00 +0000

    lazy-paginate

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,22 +1,32 @@
-#using python generator to lazy paginate
-# The goal is to not have the full list in memory, but to only have the correct page of users at a time.
-import mysql.connector
-def lazy_paginate(page_size):
-    dbConfig ={
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'bece2018',
-        'database': 'ALX_prodev'
-   }
-    conn = mysql.connector.connect(**dbConfig)
-    cursor = conn.cursor(dictionary=True)
-    try:
-        cursor.execute('SELECT * FROM user_data')
-        while True:
-            page = cursor.fetchmany(size=page_size)
-            if not page:
-                break
-            yield page
-    finally:
-        cursor.close()
-        conn.close()
+
+#!/usr/bin/env python3
+from seed import connect_to_prodev
+
+
+def paginate_users(page_size, offset):
+    connection = connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(
+        f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+def lazy_pagination(page_size):
+    """
+    Generator function to lazily load paginated data from the users database.
+    """
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
+
+
+if __name__ == "__main__":
+    for page in lazy_pagination(100):
+        for user in page:
+            print(user)
